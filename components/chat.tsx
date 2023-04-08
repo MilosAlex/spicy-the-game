b614a2808f58f71a0523767385c5a47dd27e37e3
Milos Alex <dw5pxw@inf.elte.hu>
2023-04-08 09:43:02 +0200

    feat: created styling for chat

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Chat = () => {
-  return <section className="chat">Chat</section>;
+
+  const [textValue, setTextValue] = React.useState<string>("");
+
+  const handleChatSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit");
+    setTextValue("");
+  };
+
+  return (
+    <section className="chat">
+      <div className="chat__messages-container">
+        <article className="chat__message">
+          <h4 className="chat__sender">Player1</h4>
+          <p className="chat__text">asdasd</p>
+        </article>
+        <article className="chat__message">
+          <h4 className="chat__sender">Player1</h4>
+          <p className="chat__text">asdasd</p>
+        </article>
+        <article className="chat__message">
+          <h4 className="chat__sender">Player1</h4>
+          <p className="chat__text">asdasd</p>
+        </article>
+      </div>
+      <form className="chat__form" onSubmit={handleChatSubmit}>
+        <textarea
+          className="chat__input"
+          placeholder="Type your message..."
+          value={textValue}
+          onChange={(e) => setTextValue(e.target.value)}
+        />
+        <button className="chat__button"></button>
+      </form>
+    </section>
+  );
 };
 
 export default Chat;
