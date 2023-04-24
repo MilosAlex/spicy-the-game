fc15a23981a5e86109d8a95af7f4bad5a59c10dd
Milos Alex <dw5pxw@inf.elte.hu>
2023-04-24 19:25:32 +0200

    feat: changed classnames for lobby

--- components/lobby.tsx
+++ components/lobby.tsx
@@ -11,21 +11,25 @@ interface LobbyProps {
 
 const Lobby = (props: LobbyProps) => {
   return (
-    <main className="game-room">
-      <h1 className="game-room__title">{props.title}</h1>
-      <h2 className="game-room__subtitle">Players waiting in the room:</h2>
+    <main className="lobby">
+      <h1 className="lobby__title">{props.title}</h1>
+      <h2 className="lobby__subtitle">Players waiting in the room:</h2>
       {props.players.map((player: any) => (
-        <p className="game-room__name" key={player.id}>
+        <p className="lobby__name" key={player.id}>
           {player.name}
         </p>
       ))}
-      {props.hostId.toString() === props.userId && (
-        <section className="game-room__start">
-          <button className="game-room__start-button" onClick={props.handleStartGame}>
-            Start Game
-          </button>
-        </section>
-      )}
+      {props.hostId.toString() === props.userId &&
+        props.players.length >= 2 && (
+          <section className="lobby__start">
+            <button
+              className="lobby__start-button"
+              onClick={props.handleStartGame}
+            >
+              Start Game
+            </button>
+          </section>
+        )}
     </main>
   );
 };
